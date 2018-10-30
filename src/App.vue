1638391b1986a5af15d4133d71b94ecdbfb7df74
vladtitov7 <32096809+vladtitov7@users.noreply.github.com>
2018-10-30 23:51:50 +0300

    new version of the app.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,53 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <h1 class="align-self-center">TODO list application</h1>
+     <div class="container">
+       <div class="row align-items-center">
+      <div class="col">
+        <search-task v-bind:tasks="tasks"></search-task>
+      </div>
+      <div class="col">
+        <todo-list v-bind:tasks="tasks"></todo-list>
+      </div>
+      <div class="col align-self-start">
+        <create-task @create-task="createTask"></create-task>
+      </div>
+     </div>
+     </div>
+
+  <!--  <img src="./assets/logo.png"> -->
+  <!--  <router-view/> -->
   </div>
 </template>
 
 <script>
+import sweetalert from 'sweetalert'
+import TodoList from './components/TodoList'
+import CreateTask from './components/CreateTask'
+import SearchTask from './components/SearchTask'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TodoList,
+    CreateTask,
+    SearchTask
+  },
+  data () {
+    return {
+      tasks: [{
+        title: 'Example of the task title',
+        description: 'Example of the task description',
+        status: false
+      }]
+    }
+  },
+  methods: {
+    createTask (newTask) {
+      this.tasks.push(newTask)
+      sweetalert('Success!', 'Task created', 'success')
+    }
+  }
 }
 </script>
 
@@ -21,3 +61,4 @@ export default {
   margin-top: 60px;
 }
 </style>
+
