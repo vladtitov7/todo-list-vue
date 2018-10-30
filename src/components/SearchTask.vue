39b3aa542b5e9de57ce581c4e726647833b30a92
vladtitov7 <32096809+vladtitov7@users.noreply.github.com>
2018-10-30 23:59:24 +0300

    created SearchTask.vue file
    file with "search" module

---
+++ src/components/SearchTask.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="col">
+    <div class="form-group">
+      <label for="search">Find task</label>
+        <input
+          type="text"
+          class="form-control"
+          id="search"
+          placeholder="completed, outstanding"
+          v-model="search"
+        >
+        {{ search }}
+    </div>
+    <hr>
+    <ul class="list-group">
+      <li
+        v-for="(task, index) in filteredTasks"
+        class="list-group-item list-group-item-action pointer"
+        @click="selectTask(task)"
+        v-bind:class="{'active': selectedTaskIndex === index}"
+        :key="task.id"
+      >
+      {{ task.title }}
+     </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import sweetalert from 'sweetalert'
+import Task from './Task'
+
+export default {
+  props: ['tasks', 'task'],
+  data () {
+    return {
+      search: '',
+      selectedTaskIndex: 0
+    }
+  },
+  components: {
+    Task
+  },
+  methods: {
+    selectTask (task) {
+      sweetalert(`${this.tasks.indexOf(task).title}`, 'function with description will be added as soon as possible!', 'success')
+    },
+    cancelMethod () {
+      this.taskVisibility = false
+    }
+  },
+  computed: {
+    filteredTasks () {
+      return this.tasks.filter(task => {
+        return task.title.toString().indexOf(this.search) > -1 || task.description.indexOf(this.search) > -1
+      })
+    }
+  }
+}
+</script>
